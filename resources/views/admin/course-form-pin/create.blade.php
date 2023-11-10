71a75b8cb390d3a93cd1bbbbd87191a61f3edbe0
xmartina <martinsprince126@gmail.com>
2023-11-10 14:33:51 +0000

    fixed

--- resources/views/admin/course-form-pin/create.blade.php
+++ resources/views/admin/course-form-pin/create.blade.php
@@ -1,18 +1,22 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Create Course Form Pins</title>
-</head>
-<body>
-<h1>Create Course Form Pins</h1>
+@extends('admin.layouts.master')
+@section('title', $title)
+@section('content')
 
-<form action="{{ route('pins.store') }}" method="POST">
-    @csrf
-    <label for="pin_count">Number of Pins to Generate:</label>
-    <input type="number" name="pin_count" id="pin_count" min="1" max="25" required>
-    <button type="submit">Generate Pins</button>
-</form>
-</body>
-</html>
+<!-- Start Content-->
+<div class="main-body">
+    <div class="page-wrapper">
+
+
+  <h1>Create Course Form Pins</h1>
+  
+  <form action="{{ route('pins.store') }}" method="POST">
+      @csrf
+      <label for="pin_count">Number of Pins to Generate:</label>
+      <input type="number" name="pin_count" id="pin_count" min="1" max="25" required>
+      <button type="submit">Generate Pins</button>
+  </form>
+
+
+
+
+@endsection
