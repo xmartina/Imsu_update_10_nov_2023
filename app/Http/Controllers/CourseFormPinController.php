e6ab8535d563b4fb2705becae5b86650152093ec
xmartina <martinsprince126@gmail.com>
2023-11-10 19:04:30 +0100

    dsk

--- app/Http/Controllers/CourseFormPinController.php
+++ app/Http/Controllers/CourseFormPinController.php
@@ -6,5 +6,20 @@ use Illuminate\Http\Request;
 
 class CourseFormPinController extends Controller
 {
-    //
+    // app/Http/Controllers/CourseFormPinController.php
+
+    use App\Models\CourseFormPin;
+    use App\Models\User;
+    use App\Models\Student;
+    use Illuminate\Http\Request;
+
+class CourseFormPinController extends Controller
+{
+    public function index()
+    {
+        $pins = CourseFormPin::all();
+        return view('admin.course-form-pin.index', compact('pins'));
+    }
+}
+
 }
