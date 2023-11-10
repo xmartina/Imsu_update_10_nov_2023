e3a522feb56038feb111d51527c83482f907654e
xmartina <martinsprince126@gmail.com>
2023-11-10 21:20:23 +0100

    dsk

--- app/Http/Controllers/Admin/CourseFormPinController.php
+++ app/Http/Controllers/Admin/CourseFormPinController.php
@@ -3,19 +3,18 @@
 namespace App\Http\Controllers\Admin;
   // app/Http/Controllers/CourseFormPinController.php
 
-    use App\Models\CourseFormPin;
-    use App\Http\Controllers\Controller;
-    use App\User;
-    use App\Models\Student;
-    use Illuminate\Http\Request;
-
+  use App\Models\CourseFormPin;
+  use App\Http\Controllers\Controller;
+  use App\User;
+  use App\Models\Student;
+  use Illuminate\Http\Request;
 class CourseFormPinController extends Controller
 {
     public function index()
     {
         $title = "List Pin Page";
         $pins = CourseFormPin::all();
-        return view('admin.course-form-pin.index', compact('pins'), compact('title'));
+        return view('admin.course-form-pin.index', compact('pins', 'title'));
     }
 
 }
