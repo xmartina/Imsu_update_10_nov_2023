cf6805ab78c342aaeae3f30b8662f3a095d685e2
xmartina <martinsprince126@gmail.com>
2023-11-11 06:41:06 +0100

    dsk

--- app/Models/CourseFormPin.php
+++ app/Models/CourseFormPin.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
@@ -12,6 +11,11 @@ class CourseFormPin extends Model
         'pin_num', 'is_used', 'created_admin_id',
         'date_created', 'time_created',
         'student_used_id', 'date_used', 'time_used'
+    ];
+
+    protected $rules = [
+        'pin_num' => 'required|unique:course_form_pins',
+        'is_used' => 'required|in:1,2,3',
     ];
 
     public function createdAdmin(): BelongsTo
