77956e04e2a9ddc7065b8abbd0093f4bae27622b
xmartina <martinsprince126@gmail.com>
2023-11-11 10:48:54 +0100

    dsk

--- app/Http/Controllers/Admin/CourseFormPinController.php
+++ app/Http/Controllers/Admin/CourseFormPinController.php
@@ -17,15 +17,15 @@ class CourseFormPinController extends Controller
         $title = "List Pin Page";
         $pins = CourseFormPin::with('createdAdmin')->get();
 
-        foreach ($pins as $pin) {
-            // Manually retrieve user for debugging
-            $user = UserController::find($pin->created_admin_id);
-
-            // Log or print a message for debugging
-            if ($user === null) {
-                \Log::error("User not found for CourseFormPin with ID: {$pin->id}, created_admin_id: {$pin->created_admin_id}");
-            }
-        }
+//        foreach ($pins as $pin) {
+//            // Manually retrieve user for debugging
+//            $user = UserController::find($pin->created_admin_id);
+//
+//            // Log or print a message for debugging
+//            if ($user === null) {
+//                \Log::error("User not found for CourseFormPin with ID: {$pin->id}, created_admin_id: {$pin->created_admin_id}");
+//            }
+//        }
 
 
         return view('admin.course-form-pin.index', compact('pins', 'title'));
