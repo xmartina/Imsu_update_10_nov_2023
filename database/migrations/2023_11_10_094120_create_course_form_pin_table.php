935f4443437e249dc619321137e888be1174f4b5
xmartina <martinsprince126@gmail.com>
2023-11-11 14:10:35 +0100

    dsk

--- database/migrations/2023_11_10_094120_create_course_form_pin_table.php
+++ database/migrations/2023_11_10_094120_create_course_form_pin_table.php
@@ -14,8 +14,8 @@ class CreateCourseFormPinTable extends Migration
             $table->integer('is_used')->default(2);
             $table->unsignedBigInteger('created_admin_id');
             $table->foreign('created_admin_id')->references('id')->on('admins');
-            $table->date('date_created')->useCurrentOnUpdate(now()->toDateString());
-            $table->time('time_created')->useCurrentOnUpdate();
+            $table->date('date_created')->useCurrent(now()->toDateString());
+            $table->time('time_created')->useCurrent();
             $table->unsignedBigInteger('student_used_id')->nullable();
             $table->foreign('student_used_id')->references('id')->on('students')->nullable();
             $table->date('date_used')->nullable();
